db9b8c1bac044041dd745c7543e0e4e7504cccba
dmk787 <jokpymble@gmail.com>
2025-05-27 14:51:02 +1000

    27-5-25

--- static/js/messenger.js
+++ static/js/messenger.js
@@ -28,6 +28,18 @@ window.onload = function() {
     })
     
 }
+
+const usersselected = new Map();
+
+function editusersselected(user) {
+    if (usersselected.has(user)) {
+        usersselected.delete(user);
+    }
+    else {
+        usersselected.set(user, usersselected.size);
+    }
+}
+
 async function searchusers() {
     if (document.getElementById("searchusers").value == "") {
         document.getElementById("userlist").remove();
@@ -45,26 +57,56 @@ async function searchusers() {
         let userimg = document.createElement("img");
         let usercheckbox = document.createElement("input")
         let usercheckmark = document.createElement("span");
+        let userdata = users[user]
         usercheckbox.type="checkbox";
-        userimg.src=users[user]['picture'];
-        userp.textContent=users[user]['username'];
+        userimg.src=userdata['picture'];
+        userp.textContent=userdata['username'];
         newul.appendChild(userelement);
         userelement.appendChild(userimg);
         userelement.appendChild(userp);
         userelement.appendChild(usercheckbox)
         userelement.appendChild(usercheckmark);
+        usercheckbox.onclick=function(){editusersselected(userdata['username'])};
+        if (usersselected.has(userdata['username'])) {
+            usercheckbox.checked=true;
+        }
         userelement.onclick=function(){usercheckbox.click()};
     }
     document.getElementById("newchatmenu").appendChild(newul);
     newul.id="userlist";
 }
 
-
-async function callapi(arg, val) {
-    const response = await fetch(apiurl + "?"+ String(arg) + "="+String(val));
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+async function createchat() {
+    arr = []
+    for (key of usersselected.keys()) {
+        arr.push(key)
     }
-    const json = await response.json();
-    return json;
+    callapi("createchat", true, JSON.stringify({"users": arr}))
+}
+
+
+async function callapi(arg, val, body=null) {
+    let response=""
+    if (body) {
+        response = await fetch(apiurl + "?"+ String(arg) + "="+String(val), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }        
+    }
+    else {
+        response = await fetch(apiurl + "?"+ String(arg) + "="+String(val));
+    
+    
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        return json;
+    }
 }
